b96aa795aaf3d2793db1151f2a0903ec32041ad8
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-19 05:02:49 +0300

    Add POST request

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -14,6 +17,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours, // ES6 enables us use this style <- tours: tours (same name)
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1; // Give manually id
+  const newTour = Object.assign({ id: newId }, req.body); // Merge two object
+
+  tours.push(newTour); // Add our newTour to tours
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
