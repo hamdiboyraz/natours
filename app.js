f4d73cd21ad6bbc87f55730d7d58ca9edea6096a
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-20 15:51:04 +0300

    Add config.env and dev/prod config

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1 - MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // Before this, we used to call body.parser
 app.use(express.static(`${__dirname}/public`));
 
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+// After configuration, we require app file
 const app = require('./app');
 
-const port = 3000;
+console.log(process.env.NODE_ENV);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
