f93e1f15cd41d4e1d26c4b5d9fa653ee4832a7c2
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-21 17:01:44 +0300

    Create Mongo scheme and model

--- server.js
+++ server.js
@@ -25,6 +25,26 @@ mongoose
 
 // console.log(process.env.NODE_ENV);
 
+// Creating Mongo Scheme
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // 2nd argument will show up in error
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// Creating Mongo Model based on Mongo Scheme
+const Tour = mongoose.model('Tour', tourSchema); // Model names and vaiables start with Uppercase
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
