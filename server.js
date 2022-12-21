5c880c22e1a1b24a88178f0835cfeb103428288a
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-21 16:12:11 +0300

    Connect Atlas MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 
 // After configuration, we require app file
 const app = require('./app');
 
-console.log(process.env.NODE_ENV);
+const DB = process.env.DB_URI.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the DB succesully');
+  })
+  .catch((err) => {
+    console.log(`DB connection err:, ${err}`);
+  });
+
+// console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
