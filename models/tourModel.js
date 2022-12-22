f41236c297dc3ed5487fdfc3fa69ab285ef83b50
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-22 03:35:24 +0300

    Update tour model and add import-dev-data.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,27 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'], // 2nd argument will show up in error
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   rating: {
     type: Number,
@@ -15,6 +36,27 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
 });
 
 // Creating Mongo Model based on Mongo schema
