6f2d6ee607c80d03281af65dbb2cbe7c9f6749e3
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-26 03:09:37 +0300

    Add catch unhandled rejections

--- server.js
+++ server.js
@@ -19,14 +19,28 @@ mongoose
   })
   .then(() => {
     console.log('Connected to the DB succesully');
-  })
-  .catch((err) => {
-    console.log(`DB connection err:, ${err}`);
   });
+// .catch((err) => {
+//   console.log(`DB connection err:, ${err}`);
+// });
 
 // console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Global Unhandled Rejections
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    // If we use direct process.exit-> it will sudden close that means
+    // maybe requests are currently running
+    // So we use server.close, it gives some time to finish requests
+    process.exit(1); // exit code 1 means uncaught exception
+  });
+  // Generally there is a way to restart the server.
+  // But in this case app will stay in crashed.
 });
