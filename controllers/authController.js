134ed809a3011797b256ebf9aba5e59c52a55281
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-29 17:27:00 +0300

    Add sending JWT via Cookie-HTTPonly

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,20 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000 // convert to milliseconds
+    ),
+    secure: false, // only send cookie over https
+    httpOnly: true, // cookie cannot be accessed or modified in any way by the browser -> in order to prevent cross-site scripting attacks
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
