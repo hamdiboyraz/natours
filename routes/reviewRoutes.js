6b1e6778ea4dec049e41ace8b40bf72f39b02be0
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-02 00:54:00 +0300

    Update missed authentication and authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -9,11 +9,13 @@ const router = express.Router({ mergeParams: true });
 // POST /tours/:tourId/reviews
 // POST /reviews
 // Above two routes are same, redirected from tourRoutes.js
+
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -22,7 +24,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,23 +8,20 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch(
-  '/changePassword',
-  authController.protect,
-  authController.updatePassword
-);
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/changePassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// Restrict all routes after this middleware to admin only
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
