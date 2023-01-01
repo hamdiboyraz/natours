7014728bd51a7832fed76df01942af68c85c3947
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-02 01:08:55 +0300

    Add user and review data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
 const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 dotenv.config({ path: `${__dirname}/../../config.env` });
 
@@ -25,11 +26,17 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false }); // Also close encryption in user model
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -45,6 +52,14 @@ const deleteData = async () => {
       console.log('Data successfully deleted!');
     } else if (process.argv[3] === '--users') {
       await User.deleteMany();
+      console.log('Data successfully deleted!');
+    } else if (process.argv[3] === '--reviews') {
+      await Review.deleteMany();
+      console.log('Data successfully deleted!');
+    } else {
+      await Tour.deleteMany();
+      await User.deleteMany();
+      await Review.deleteMany();
       console.log('Data successfully deleted!');
     }
   } catch (err) {
