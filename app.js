6bdcb3ce2dba85715070ce31397e34d4d07ef810
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-04 00:36:23 +0300

    Extend our base template with blocks

--- app.js
+++ app.js
@@ -72,13 +72,25 @@ app.use((req, res, next) => {
 });
 
 // 3 - ROUTE
-// Mounting routes
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
     user: ' Jonas',
   });
 });
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+  });
+});
+// Mounting routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
