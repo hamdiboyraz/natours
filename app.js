993c8a23b358373e7287d39f97ded5aba5b549cf
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-05 00:34:25 +0300

    Make basic post request with axios and get token from cookie

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -72,6 +73,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); // Before .json, we used to body.parser prevşiously
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -97,6 +99,7 @@ app.use(
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); // ISOString nicely formatted at date
   // console.log(req.headers);
+  console.log(req.cookies);
   next();
 });
 
