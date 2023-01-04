f0e99b06333042f9fe9bed78d9f118d090b6c228
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-04 17:47:51 +0300

    Add a map with mapbox

--- app.js
+++ app.js
@@ -26,7 +26,35 @@ app.use(express.static(`${__dirname}/public`));
 // 1 - GLOBAL MIDDLEWARES
 
 // Set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'worker-src': ['blob:'],
+        'child-src': ['blob:', 'https://js.stripe.com/'],
+        'img-src': ["'self'", 'data: image/webp'],
+        'script-src': [
+          "'self'",
+          'https://api.mapbox.com',
+          'https://cdnjs.cloudflare.com',
+          'https://js.stripe.com/v3/',
+          "'unsafe-inline'",
+        ],
+        'connect-src': [
+          "'self'",
+          'ws://localhost:*',
+          'ws://127.0.0.1:*',
+          'http://127.0.0.1:*',
+          'http://localhost:*',
+          'https://*.tiles.mapbox.com',
+          'https://api.mapbox.com',
+          'https://events.mapbox.com',
+        ],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+  })
+);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
---
+++ public/js/mapbox.js
@@ -0,0 +1,58 @@
+import 'https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js';
+
+import token from './mapbox_token.js';
+
+// get locations from map element
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
+
+// Display map on page with mapbox
+mapboxgl.accessToken = token;
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/devnew/clchnzcit008f14qi1jx5t318',
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 5,
+  scrollZoom: false,
+});
+
+// Define bounds for map to fit
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker to map
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom', // bottom of the marker
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30, // offset from the marker preventing popup from covering the marker
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+// Fit map to bounds, automatically zooming in and out
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
+
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
