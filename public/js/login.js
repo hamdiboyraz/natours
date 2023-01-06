86d9e5b67aeca8794a3ea6041e78ac4b8cd94531
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-06 03:07:22 +0300

    Add login feature and showing alert

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+import { showAlert } from './alert.js';
+
 const login = async (email, password) => {
   try {
     const res = await axios({
@@ -10,19 +12,23 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in successfully!');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+const loginForm = document.querySelector('.form');
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
