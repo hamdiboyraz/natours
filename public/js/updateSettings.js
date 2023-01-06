1df8e7c77b51c082110074a2dc17a5c73110503f
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-07 01:09:00 +0300

    Add update user password

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,18 +1,20 @@
 import { showAlert } from './alert.js';
 
-const updateData = async (name, email) => {
+// type is either 'password' or 'data'
+const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://localhost:3000/api/v1/users/changePassword'
+        : 'http://localhost:3000/api/v1/users/updateMe';
     const res = await axios({
       method: 'PATCH',
-      url: 'http://localhost:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
       window.setTimeout(() => {
         location.assign('/me');
       }, 1500);
@@ -22,13 +24,33 @@ const updateData = async (name, email) => {
   }
 };
 
-const updateSettingsForm = document.querySelector('.form-user-data');
+const updateDataForm = document.querySelector('.form-user-data');
+const updatePasswordForm = document.querySelector('.form-user-password');
 
-if (updateSettingsForm) {
-  updateSettingsForm.addEventListener('submit', (e) => {
+if (updateDataForm) {
+  updateDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (updatePasswordForm) {
+  updatePasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    console.log(passwordCurrent, password, passwordConfirm);
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
