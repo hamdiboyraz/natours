d75dbb58a01a17c9be9cd8f65c7d57bfbe8700f7
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-07 00:11:05 +0300

    Add update user data with API

---
+++ public/js/updateSettings.js
@@ -0,0 +1,34 @@
+import { showAlert } from './alert.js';
+
+const updateData = async (name, email) => {
+  try {
+    const res = await axios({
+      method: 'PATCH',
+      url: 'http://localhost:3000/api/v1/users/updateMe',
+      data: {
+        name,
+        email,
+      },
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Data updated successfully!');
+      window.setTimeout(() => {
+        location.assign('/me');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
+const updateSettingsForm = document.querySelector('.form-user-data');
+
+if (updateSettingsForm) {
+  updateSettingsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateData(name, email);
+  });
+}
