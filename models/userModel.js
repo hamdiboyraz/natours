d8bd5b8536b354d567cd494a397c470f621396c6
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-07 17:49:52 +0300

    Save image name to DB

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
     lowercasse: true,
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  photo: String,
+  photo: { type: String, default: 'default.jpg' },
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
