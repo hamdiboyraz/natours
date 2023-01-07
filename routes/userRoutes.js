ea5f96f00843c3cce916a5c5d861dec82162ea86
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-07 15:02:02 +0300

    Add multer and basic upload

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
+
+// if we don't specify a destination, multer will store the file in memory
+const upload = multer({ dest: 'public/img/users' });
 
 const router = express.Router();
 
@@ -18,7 +22,7 @@ router.use(authController.protect);
 router.patch('/changePassword', authController.updatePassword);
 
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.delete('/deleteMe', userController.deleteMe);
 
 // Restrict all routes after this middleware to admin only
