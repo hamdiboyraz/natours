f32a3eb89e2efa4e503ec635431907648808c53e
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-01-10 02:59:05 +0300

    Add payments on frontend

---
+++ public/js/stripe.js
@@ -0,0 +1,28 @@
+import stripe_token from './stripe_token.js';
+import { showAlert } from './alert.js';
+
+const stripe = Stripe(stripe_token);
+
+export const bookTour = async (tourID) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourID}`
+    );
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
+
+const bookBtn = document.getElementById('book-tour');
+if (bookBtn)
+  bookBtn.addEventListener('click', (e) => {
+    e.target.textContent = 'Processing...';
+    const tourId = e.target.dataset.tourId; // const {tourId = e.target.dateset}
+    bookTour(tourId);
+  });
